47498d8339ac1b51894d7bbeab721de4b54083c0
leo <leochan1011@github.com>
2020-04-22 22:29:54 +0800

    go

---
+++ app.js
@@ -0,0 +1,89 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction()
+};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+//slider
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("slides_dot");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+// text content of character info
+contentIndex = 0;
+var content = [
+  "intro_murada",
+  "intro_ozaki",
+  "intro_kanao",
+  "intro_chutaro"
+]
+
+// Text Content Change
+
+//change current content 
+function introContentAdd(n) {
+  contentIndex += n;
+
+  if (contentIndex >= content.length) {
+    contentIndex = 0;
+  } else if (contentIndex < 0) {
+    contentIndex = content.length - 1;
+  }
+
+  showContent(contentIndex);
+}
+
+//change current content
+function introContentCurrent(n) {
+  showContent(n);
+}
+
+function showContent(n) {
+  for (var i = 0; i < content.length; i++) {
+    document.getElementById(content[i]).style.display = "none";
+  }
+  document.getElementById(content[n]).style.display = "block"
+}
