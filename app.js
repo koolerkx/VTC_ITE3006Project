ac962ef8e7e82b8ed273d2ddd78c6584f6688154
kmcazee <kmcazee@gmail.com>
2020-04-23 05:58:54 +0800

    Index Update
    Finish the index page

--- app.js
+++ app.js
@@ -1,89 +1,108 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () {
-  myFunction()
-};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
+// SlideShow JS Start
 //slider
 var slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
+    introContentAdd(n)
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
+    introContentCurrent(n-1);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("slides_dot");
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+    var i;
+    var slides = document.getElementsByClassName("character_img");
+    var dots = document.getElementsByClassName("slides_dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
+// SlideShow JS END
 
 // text content of character info
 contentIndex = 0;
 var content = [
-  "intro_murada",
-  "intro_ozaki",
-  "intro_kanao",
-  "intro_chutaro"
+    "intro_murada",
+    "intro_ozaki",
+    "intro_kanao",
+    "intro_chutaro"
 ]
+showContent(0)
 
-// Text Content Change
-
-//change current content 
 function introContentAdd(n) {
-  contentIndex += n;
+    contentIndex += n;
 
-  if (contentIndex >= content.length) {
-    contentIndex = 0;
-  } else if (contentIndex < 0) {
-    contentIndex = content.length - 1;
-  }
+    if (contentIndex >= content.length) {
+        contentIndex = 0;
+    } else if (contentIndex < 0) {
+        contentIndex = content.length - 1;
+    }
 
-  showContent(contentIndex);
+    showContent(contentIndex);
 }
 
-//change current content
 function introContentCurrent(n) {
-  showContent(n);
+    contentIndex = n;
+    showContent(contentIndex);
 }
 
 function showContent(n) {
-  for (var i = 0; i < content.length; i++) {
-    document.getElementById(content[i]).style.display = "none";
-  }
-  document.getElementById(content[n]).style.display = "block"
+    for (var i = 0; i < content.length; i++) {
+        document.getElementById(content[i]).style.display = "none";
+        document.getElementById(content[i]).style.opacity = 0;
+    }
+
+    document.getElementById(content[n]).style.display = "block"
+    document.getElementById(content[n]).style.opacity = 1;
 }
+
+// Back to top Button function
+/* Ref: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp */
+backToTopBtn = document.getElementById("backToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopBtn.style.display = "block";
+    // backToTopBtn.style.opacity = 1;
+  } else {
+    backToTopBtn.style.display = "none";
+    // backToTopBtn.style.opacity = 0;
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function backToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
